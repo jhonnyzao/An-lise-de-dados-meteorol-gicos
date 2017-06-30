01853cb27227e0049b63b192248e5c1c5982f648
guilherme <guilherme@guilherme-370E4K>
2017-06-30 15:26:50 -0300

    Iniciado projeto maven de exemplo

---
+++ src/main/java/SD_EP2/SD_EP2_maven/App.java
@@ -0,0 +1,13 @@
+package SD_EP2.SD_EP2_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
