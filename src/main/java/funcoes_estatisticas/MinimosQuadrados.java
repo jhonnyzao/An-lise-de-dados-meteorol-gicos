6ed7a11b81840509d23a4766923786694d1cafeb
Gabriel <gabriel.piotogomes@gmail.com>
2017-07-01 23:37:39 -0300

    iniciado o desenvolvimento dos minimos quadrados

--- src/main/java/funcoes_estatisticas/MinimosQuadrados.java
+++ src/main/java/funcoes_estatisticas/MinimosQuadrados.java
@@ -1,7 +1,59 @@
 package funcoes_estatisticas;
 
-public class MinimosQuadrados {
+import java.util.Iterator;
+import org.apache.hadoop.io.FloatWritable;
 
-	// TODO
+public class MinimosQuadrados {
+	
+	private Iterator<FloatWritable> values;
+	private double a, b;
+	private float mediaX, mediaY;
+	private Dados dados;
+	
+	public MinimosQuadrados(Iterator<FloatWritable> values) {
+		this.values = values;
+		dados = Media.mediaPasso(values);
+		this.b = calcularB();
+		this.a = calcularA();
+	}
+
+	public Double calcularB() {
+		int x = 1;
+		double somatoriaNumerador = 0;
+		double somatoriaDenominador = 0;
+		mediaY = dados.getMedia();
+		
+		int n = dados.getElementos().size();
+		
+		mediaX = (n+1)/2;
+		
+		while(values.hasNext()) {
+			somatoriaNumerador += x * (values.next().get() - mediaY);
+			somatoriaDenominador += x * (x - mediaX);
+			x++;
+		}
+		
+		return somatoriaNumerador / somatoriaDenominador;
+	}
+	
+	public Double calcularA() {
+		return mediaY - b * mediaX;
+	}
+	
+	public double getA() {
+		return a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	
+	public double getXMin() {
+		return 1;
+	}
+	
+	public double getXMax() {
+		return dados.getElementos().size();
+	}
 	
 }
--- src/main/java/reducers/TemperaturaMinimosQuadradosReducer.java
+++ src/main/java/reducers/TemperaturaMinimosQuadradosReducer.java
@@ -1,7 +1,6 @@
 package reducers;
 
 import java.io.IOException;
-
 import java.util.Iterator;
 
 import org.apache.hadoop.io.DoubleWritable;
@@ -12,8 +11,12 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
+import funcoes_estatisticas.MinimosQuadrados;
+
 public class TemperaturaMinimosQuadradosReducer implements Reducer<Text, FloatWritable, Text, DoubleWritable> {
 
+	private int eixoX = 1;
+	
 	public void configure(JobConf arg0) {
 		// TODO Auto-generated method stub
 
@@ -24,10 +27,15 @@ public class TemperaturaMinimosQuadradosReducer implements Reducer<Text, FloatWr
 
 	}
 
-	public void reduce(Text arg0, Iterator<FloatWritable> arg1, OutputCollector<Text, DoubleWritable> arg2,
-			Reporter arg3) throws IOException {
-
-		// TODO
+	public void reduce(Text key, Iterator<FloatWritable> values, OutputCollector<Text, DoubleWritable> output,
+			Reporter reporter) throws IOException {
+		
+		MinimosQuadrados min = new MinimosQuadrados(values);
+		double y0 = min.getA() + min.getB() * min.getXMin();
+		double y1 = min.getA() + min.getB() * min.getXMax();
+		
+		output.collect(new Text("y0"), new DoubleWritable(y0));
+		output.collect(new Text("y1"), new DoubleWritable(y1));
 
 	}
 
